dc37881e85b3e5c2497306a7da9cbee2864e1cb4
David Villalba Pereira <vipereira.david@gmail.com>
2025-10-04 19:43:44 -0400

    Added Lab 5

--- Lab5.py
+++ Lab5.py
@@ -5,18 +5,52 @@
 # *   *
 # *****
 def hollow_square(n):
-    return ""
-
+    shape = ""
+    if n >= 1:
+        for i in range(n):
+            shape += "*"
+        shape += "\n"
+    for i in range(n - 2):
+        shape += "*"
+        for i in range(n - 2):
+            shape += " "
+        shape += "*\n"
+    if n > 1:
+        for i in range(n):
+            shape += "*"
+    return shape.rstrip()
+   
 # 1
 # 12
 # 123
 # 1234
 def number_pattern(n):
-    return ""
+    shape = ""
+    i = 1
+
+    while i <= n:
+        j = 1
+        line = ""
+
+        while j <= i:
+            line += str(j)
+            j += 1
+
+        shape += line + "\n"
+        i += 1
+
+    return shape.rstrip()
 
 # Example: For n = 5, sum = 1 + 2 + 3 + 4 + 5 = 15
 def sum_of_natural_numbers(n):
-    return ""
+    total = 0 
+    i = 1
+
+    while i <= n:
+        total += i
+        i += 1
+
+    return total
 
 # Example for n = 4:
 #    *
@@ -24,4 +58,24 @@ def sum_of_natural_numbers(n):
 #  *****
 # *******
 def centered_star_pyramid(n):
-    return ""
+    shape = ""
+    i = 1
+
+    while i <= n:
+        spaces_count = n - i
+        j = 0 
+        while j < spaces_count:
+            shape += " "
+            j += 1
+
+        stars_count = 2 * i - 1
+        k = 0
+        while k < stars_count:
+            shape += "*"
+            k += 1
+
+        if i < n:
+            shape += "\n"
+        i += 1
+
+    return shape
